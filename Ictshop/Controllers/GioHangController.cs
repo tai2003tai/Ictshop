1c015e72c721a3af7a5d916215c7aae90ba8e861
tai2003tai <nguyentai290290@gmail.com>
2023-11-29 19:11:38 +0700

    Add project files.

---
+++ Ictshop/Controllers/GioHangController.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+using PayPal.Api;
+
+namespace Ictshop.Controllers
+{
+    public class GioHangController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        // GET: GioHang
+        
+        //Lấy giỏ hàng 
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                //Nếu giỏ hàng chưa tồn tại thì mình tiến hành khởi tao list giỏ hàng (sessionGioHang)
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        //Thêm giỏ hàng
+        public ActionResult ThemGioHang(int iMasp, string strURL)
+        {
+            Sanpham sp = db.Sanphams.SingleOrDefault(n => n.Masp == iMasp);
+            if ( sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy ra session giỏ hàng
+            List<GioHang> lstGioHang = LayGioHang();
+            //Kiểm tra sp này đã tồn tại trong session[giohang] chưa
+            GioHang sanpham = lstGioHang.Find(n => n.iMasp == iMasp);
+            if (sanpham == null)
+            {
+                sanpham = new GioHang(iMasp);
+                //Add sản phẩm mới thêm vào list
+                lstGioHang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoLuong++;
+                return Redirect(strURL);
+            }
+        }
+        //Cập nhật giỏ hàng 
+        public ActionResult CapNhatGioHang(int iMaSP, FormCollection f)
+        {
+            //Kiểm tra masp
+            Sanpham sp = db.Sanphams.SingleOrDefault(n => n.Masp== iMaSP);
+            //Nếu get sai masp thì sẽ trả về trang lỗi 404
+            if (sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy giỏ hàng ra từ session
+            List<GioHang> lstGioHang = LayGioHang();
+            //Kiểm tra sp có tồn tại trong session["GioHang"]
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.iMasp == iMaSP);
+            //Nếu mà tồn tại thì chúng ta cho sửa số lượng
+            if (sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(f["txtSoLuong"].ToString());
+
+            }
+            return RedirectToAction("GioHang");
+        }
+        //Xóa giỏ hàng
+        public ActionResult XoaGioHang(int iMaSP)
+        {
+            //Kiểm tra masp
+            Sanpham sp = db.Sanphams.SingleOrDefault(n => n.Masp== iMaSP);
+            //Nếu get sai masp thì sẽ trả về trang lỗi 404
+            if (sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy giỏ hàng ra từ session
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.iMasp == iMaSP);
+            //Nếu mà tồn tại thì chúng ta cho sửa số lượng
+            if (sanpham != null)
+            {
+                lstGioHang.RemoveAll(n => n.iMasp == iMaSP);
+
+            }
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("GioHang");
+        }
+        //Xây dựng trang giỏ hàng
+        public ActionResult GioHang()
+        {
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            List<GioHang> lstGioHang = LayGioHang();
+            return View(lstGioHang);
+        }
+        //Tính tổng số lượng và tổng tiền
+        //Tính tổng số lượng
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongSoLuong = lstGioHang.Sum(n => n.iSoLuong);
+            }
+            return iTongSoLuong;
+        }
+        //Tính tổng thành tiền
+        private double TongTien()
+        {
+            double dTongTien = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                dTongTien = lstGioHang.Sum(n => n.ThanhTien);
+            }
+            return dTongTien;
+        }
+        //tạo partial giỏ hàng
+        public ActionResult GioHangPartial()
+        {
+            if (TongSoLuong() == 0)
+            {
+                return PartialView();
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return PartialView();
+        }
+        //Xây dựng 1 view cho người dùng chỉnh sửa giỏ hàng
+        public ActionResult SuaGioHang()
+        {
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            List<GioHang> lstGioHang = LayGioHang();
+            return View(lstGioHang);
+
+        }
+
+        #region // Mới hoàn thiện
+        //Xây dựng chức năng đặt hàng
+        [HttpPost]
+        public ActionResult DatHang(FormCollection donhangForm)
+        {
+            //Kiểm tra đăng đăng nhập
+            if (Session["use"] == null || Session["use"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "User");
+            }
+            //Kiểm tra giỏ hàng
+            if (Session["GioHang"] == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+            Console.WriteLine(donhangForm);
+            string diachinhanhang = donhangForm["Diachinhanhang"].ToString();
+            string thanhtoan = donhangForm["MaTT"].ToString();
+            int ptthanhtoan = Int32.Parse(thanhtoan);
+
+            //Thêm đơn hàng
+            Donhang ddh = new Donhang();
+            Nguoidung kh = (Nguoidung)Session["use"];
+            List<GioHang> gh = LayGioHang();
+            ddh.MaNguoidung = kh.MaNguoiDung;
+            ddh.Ngaydat = DateTime.Now;
+            ddh.Thanhtoan = ptthanhtoan;
+            ddh.Diachinhanhang = diachinhanhang;
+            decimal tongtien = 0;
+            foreach (var item in gh)
+            {
+                decimal thanhtien = item.iSoLuong * (decimal)item.dDonGia;
+                tongtien += thanhtien;
+            }
+            ddh.Tongtien = tongtien;
+            db.Donhangs.Add(ddh);
+            db.SaveChanges();
+            //Thêm chi tiết đơn hàng
+            foreach (var item in gh)
+            {
+                Chitietdonhang ctDH = new Chitietdonhang();
+                decimal thanhtien =  item.iSoLuong * (decimal) item.dDonGia;
+                ctDH.Madon = ddh.Madon;
+                ctDH.Masp = item.iMasp;
+                ctDH.Soluong = item.iSoLuong;
+                ctDH.Dongia = (decimal)item.dDonGia;
+                ctDH.Thanhtien = (decimal) thanhtien;
+                ctDH.Phuongthucthanhtoan = 1;
+                db.Chitietdonhangs.Add(ctDH);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index", "Donhangs");
+        }
+        #endregion
+
+        public ActionResult ThanhToanDonHang()
+        {
+
+            ViewBag.MaTT = new SelectList(new[]
+                {
+                    new { MaTT = 1, TenPT="Thanh toán tiền mặt" },
+                    new { MaTT = 2, TenPT="Thanh toán chuyển khoản" },
+                }, "MaTT", "TenPT", 1);
+            ViewBag.MaNguoiDung = new SelectList(db.Nguoidungs, "MaNguoiDung", "Hoten");
+
+            //Kiểm tra đăng đăng nhập
+            if (Session["use"] == null || Session["use"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "User");
+            }
+            //Kiểm tra giỏ hàng
+            if (Session["GioHang"] == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+            //Thêm đơn hàng
+            Donhang ddh = new Donhang();
+            Nguoidung kh = (Nguoidung)Session["use"];
+            List<GioHang> gh = LayGioHang();
+            decimal tongtien = 0;
+            foreach (var item in gh)
+            {
+                decimal thanhtien = item.iSoLuong * (decimal)item.dDonGia;
+                tongtien += thanhtien;
+            }
+
+            ddh.MaNguoidung = kh.MaNguoiDung;
+            ddh.Ngaydat = DateTime.Now;
+            Chitietdonhang ctDH = new Chitietdonhang();
+            ViewBag.tongtien = tongtien;
+            return View(ddh);
+
+        }
+        public ActionResult PaymentWithPaypal(string Cancel = null)
+        {
+            //getting the apiContext  
+            APIContext apiContext = PaypalConfiguration.GetAPIContext();
+            try
+            {
+                //A resource representing a Payer that funds a payment Payment Method as paypal  
+                //Payer Id will be returned when payment proceeds or click to pay  
+                string payerId = Request.Params["PayerID"];
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    //this section will be executed first because PayerID doesn't exist  
+                    //it is returned by the create function call of the payment class  
+                    // Creating a payment  
+                    // baseURL is the url on which paypal sendsback the data.  
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority + "/GioHang/PaymentWithPayPal?";
+                    //here we are generating guid for storing the paymentID received in session  
+                    //which will be used in the payment execution  
+                    var guid = Convert.ToString((new Random()).Next(100000));
+                    //CreatePayment function gives us the payment approval url  
+                    //on which payer is redirected for paypal account payment  
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
+                    //get links returned from paypal in response to Create function call  
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = null;
+                    while (links.MoveNext())
+                    {
+                        Links lnk = links.Current;
+                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            //saving the payapalredirect URL to which user will be redirected for payment  
+                            paypalRedirectUrl = lnk.href;
+                        }
+                    }
+                    // saving the paymentID in the key guid  
+                    Session.Add(guid, createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    // This function exectues after receving all parameters for the payment  
+                    var guid = Request.Params["guid"];
+                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
+                    //If executed payment failed then we will show payment failure message to user  
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("FailureView");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("FailureView");
+            }
+            //on successful payment, show success page to user.  
+            return View("SuccessView");
+        }
+        private PayPal.Api.Payment payment;
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+            this.payment = new Payment()
+            {
+                id = paymentId
+            };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl)
+        {
+            //create itemlist and add item objects to it  
+            var itemList = new ItemList()
+            {
+                items = new List<Item>()
+            };
+            //Adding Item Details like name, currency, price etc  
+            itemList.items.Add(new Item()
+            {
+                name = "Item Name comes here",
+                currency = "USD",
+                price = "1",
+                quantity = "1",
+                sku = "sku"
+            });
+            var payer = new Payer()
+            {
+                payment_method = "paypal"
+            };
+            // Configure Redirect Urls here with RedirectUrls object  
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl + "&Cancel=true",
+                return_url = redirectUrl
+            };
+            // Adding Tax, shipping and Subtotal details  
+            var details = new Details()
+            {
+                tax = "1",
+                shipping = "1",
+                subtotal = "1"
+            };
+            //Final amount with details  
+            var amount = new Amount()
+            {
+                currency = "USD",
+                total = "3", // Total must be equal to sum of tax, shipping and subtotal.  
+                details = details
+            };
+            var transactionList = new List<Transaction>();
+            // Adding description about the transaction  
+            var paypalOrderId = DateTime.Now.Ticks;
+            transactionList.Add(new Transaction()
+            {
+                description = $"Invoice #{paypalOrderId}",
+                invoice_number = paypalOrderId.ToString(), //Generate an Invoice No    
+                amount = amount,
+                item_list = itemList
+            });
+            this.payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+            // Create a payment using a APIContext  
+            return this.payment.Create(apiContext);
+        }
+
+
+    }
+}
